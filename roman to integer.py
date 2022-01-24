9e15580ab57f9050d1e975f4fb0f891d312ce348
Vkumar, Chitra <charucvk@gmail.com>
2022-01-24 18:24:58 +0530

    Completed the code

--- roman to integer.py
+++ roman to integer.py
@@ -2,7 +2,17 @@ def romanToInt( s: str):
     # s = "MCMXCIV"
     table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+    x = 0
+
     for i in range(len(s)):
-        if 
+        if i + 1 < len(s) and table[s[i]] < table[s[i+1]]:
+            x -= table[s[i]]
+        else:
+            x += table[s[i]]
+
+    return x
+
+print(romanToInt("MCMXCIV"))
+
 
 
